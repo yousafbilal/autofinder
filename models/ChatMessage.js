ce058e010a628980e1aac6299e36d61ba4cce656
Ashfaq2681 <yousaf.b666@gmail.com>
2026-03-06 11:54:08 +0500

    first commit

--- models/ChatMessage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatMessageSchema = new mongoose.Schema({
-  conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'ChatConversation', required: true },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('ChatMessage', ChatMessageSchema);
